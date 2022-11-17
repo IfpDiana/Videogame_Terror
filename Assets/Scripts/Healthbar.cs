9bbd7ef8e2e8194d31bff3b9b2163af8f6da8fd0
IfpDiana <dianayamaroto@gmail.com>
2022-11-17 13:03:04 +0100

    Code_Life

---
+++ Assets/Scripts/Healthbar.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Healthbar : MonoBehaviour //este código, se le asigna al jugador.
+{
+    public Image healthbar; //Le dices a la imagen que se ponga, teniendo esta el nombre de "Barra de vida"
+    public float life = 100; //Creas una variable de "vida" con un máximo de está.
+
+    void Update()
+    {
+        life = Mathf.Clamp(life, 0, 100); //hace que nuestra vida no pase del máximo ni del mínimo.
+        healthbar.fillAmount = life / 100;
+    }
+}
