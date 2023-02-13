7e467e42e6ff206a10011d7c223e51e4d4a0d688
Nach25 <nachomisternacho@gmail.com>
2023-02-13 20:45:01 +0100

    Keypad

---
+++ Assets/Scripts/Keypad.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Keypad : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI Ans;
+    [SerializeField] private Animator Door;
+    private bool ItsAtDoor = false;
+    public GameObject CodePanel;
+
+    private string Answer = "1234";
+
+    public void Number(int number)
+    {
+        Ans.text += number.ToString();
+    }
+
+    public void Intro()
+    {
+        if (Ans.text == Answer)
+        {
+            Ans.text = "Correct";
+            Door.SetBool("Open", true);
+            StartCoroutine("StopDoor");
+        }
+        else
+        {
+            Ans.text = "Invalid";
+        }
+    }
+
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.E) && ItsAtDoor == true)
+        {
+            CodePanel.SetActive(true);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            ItsAtDoor = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        ItsAtDoor = false;
+        CodePanel.SetActive(false);
+    }
+
+    IEnumerator StopDoor()
+    {
+        yield return new WaitForSeconds(0.5f);
+        Door.SetBool("Open", false);
+        Door.enabled = false;
+    }
+
+    public void Clear()
+    {
+        Ans.text = "";
+    }
+}
